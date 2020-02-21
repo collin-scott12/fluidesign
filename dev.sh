104cef4dd40c1d2a9fa38e0ac91e80a45e7c6146
Collin Scott <cscott@Collins-MacBook-Pro-5.local>
2020-02-21 17:29:00 -0500

    Continued refinements for future development

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Prepares Sass and HTML for Testing
-sass --watch test-file.sass test-file.css
-fswatch -o ~/Desktop/Dev/framework | xargs -n1 -I {} osascript -e 'tell application "Google Chrome" to tell the active tab of its first window to reload'
+sass --watch fluidesign.sass fluidesign.css &
+http-server
